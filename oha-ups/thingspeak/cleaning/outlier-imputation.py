b6fa1b28e07f3283b01a58c09a5e9227ab818235
Ian Ozeroff <ian.ozeroff13@gmail.com>
2020-02-11 09:23:58 -0500

    Deleted holdover python code.

--- oha-ups/thingspeak/cleaning/outlier-imputation.py
+++
@@ -1,4 +0,0 @@
-# This file prepares the outlier handling function for our data.
-# TODO: Would like to use the MazamaCoreUtils
-#  AirSensor package in R. This issue with this is, you need a pat object to pass to pat_outliers(), but I would like
-#  to pass the data ingested by this package.
