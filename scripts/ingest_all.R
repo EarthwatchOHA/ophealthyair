abde26ca451086ff911c14a31783ca47b449823b
Ian Ozeroff <ian.ozeroff13@gmail.com>
2020-04-27 01:19:30 -0400

    AirSensor local fork unreliability workaround.

--- scripts/ingest_all.R
+++ scripts/ingest_all.R
@@ -1,12 +1,8 @@
 library(MazamaCoreUtils)
 library(MazamaSpatialUtils)
-devtools::load_all("C://Users/iozeroff/Data-Science/R-Projects/AirSensor")
-devtools::load_all("C://Users/iozeroff/Data-Science/ophealthyair")
+library(AirSensor)
+devtools::load_all()
 library(dplyr)
-
-#TODO: Create way to pass in start and end date.(Functionalize?)
-# This script creates a new pas for all of the countries we operate in.
-# ? Command Line ops?
 
 setArchiveBaseUrl("http://smoke.mazamascience.com/data/PurpleAir")
 setSpatialDataDir("data/spatial")
@@ -18,7 +14,7 @@ countries_coded <- c("United States", "India", "Sri Lanka") %>%
 # TODO: Add way to specify program.
 sensor_catalog <- fetch_SensorCatalog() %>% filter(site != "Undeployed")
 
-pas <- fetch_pas(countryCodes = countries_coded, lookbackDays = 21)
+pas <- fetch_pas(countryCodes = countries_coded, lookbackDays = 30)
 
 # Setting up startdate and enddate variables.
 default_startdate <- 20180101
