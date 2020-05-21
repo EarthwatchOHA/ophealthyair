aea4ad7aab2571b2550d220ebc6beb4889900c83
Ian Ozeroff <ian.ozeroff13@gmail.com>
2020-05-21 12:46:37 -0400

    Removing vestigial files and updating names.

--- R/con2aqi.R
+++
@@ -1,5 +0,0 @@
-con2aqi <- function(cons, pollutant, over_replace_value, type = NULL, na.ignore = TRUE) {
-  out <- sapply(cons, con2aqi_single, pollutant = pollutant, over_replace_value = over_replace_value,
-                type = type, na.ignore = na.ignore)
-  return(out)
-}
